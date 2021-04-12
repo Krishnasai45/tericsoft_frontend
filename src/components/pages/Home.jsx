a7933c4ac36e6c93bdf1e0a18bd6dd091f0fc89f
Krishnasai45 <yamanikrishnasai45@gmail.com>
2021-04-12 14:46:38 +0530

    Added changes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,6 +7,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import {  Button } from "@material-ui/core";
 import {  Typography } from "@material-ui/core";
 import { useHistory, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
@@ -98,6 +99,8 @@ export function Home() {
             <TableRow>
               <StyledTableCell align="center">Sl.No</StyledTableCell>
               <StyledTableCell align="center">Movie Name</StyledTableCell>
+              <StyledTableCell align="center">Year</StyledTableCell>
+              <StyledTableCell align="center">Genre</StyledTableCell>
               <StyledTableCell align="center">Edit</StyledTableCell>
               <StyledTableCell align="center">Delete</StyledTableCell>
             </TableRow>
@@ -108,15 +111,17 @@ export function Home() {
                 <StyledTableRow key={item._id}>
                   <StyledTableCell align="center">{index+1}</StyledTableCell>
                   <StyledTableCell align="center" onClick = {()=>handleMovie(item._id)}>{item.name}</StyledTableCell>
-                  <StyledTableCell align="center"><button onClick={()=>handleEditClick(item._id)} >Edit</button></StyledTableCell>
-                  <StyledTableCell align="center"><button onClick = {()=>handleDelete(item._id)}>Delete</button></StyledTableCell>
+                  <StyledTableCell align="center" onClick = {()=>handleMovie(item._id)}>{item.year}</StyledTableCell>
+                  <StyledTableCell align="center" onClick = {()=>handleMovie(item._id)}>{item.genre}</StyledTableCell>
+                  <StyledTableCell align="center"><Button onClick={()=>handleEditClick(item._id)} >Edit</Button></StyledTableCell>
+                  <StyledTableCell align="center"><Button onClick = {()=>handleDelete(item._id)}>Delete</Button></StyledTableCell>
                 </StyledTableRow>
               )
             )}
           </TableBody>
         </Table>
       </TableContainer>
-        
+
     </>
   );
 }
